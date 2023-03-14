8914858e8e398089cabf1abcf3c4fe2ad8b95b12
21520147-PhamTuanAnh <21520147@gm.uit.edu.vn>
2023-03-15 01:45:48 +0700

    setup hasura graphql #2
    Signed-off-by: 21520147-PhamTuanAnh <21520147@gm.uit.edu.vn>

--- app/src/main/java/com/example/hotelmanagement/MainActivity.java
+++ app/src/main/java/com/example/hotelmanagement/MainActivity.java
@@ -2,7 +2,14 @@ package com.example.hotelmanagement;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.apollographql.apollo.ApolloCall;
+import com.apollographql.apollo.api.Response;
+import com.apollographql.apollo.exception.ApolloException;
+import com.example.hasura.Hasura;
+import com.example.hasura.UniqueQuery;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+
+        //region Hasura Testing
+        Thread thread = new Thread(() -> {
+            Hasura.apolloClient.query(new UniqueQuery()).enqueue(new ApolloCall.Callback<UniqueQuery.Data>() {
+                @Override
+                public void onResponse(@NonNull Response<UniqueQuery.Data> response) {
+                    System.out.println(response.getData().UNIQUE());
+                }
+
+                @Override
+                public void onFailure(@NonNull ApolloException e) {
+                    System.out.println(e);
+                }
+            });
+        });
+        thread.start();
+        //endregion
     }
+
 }
