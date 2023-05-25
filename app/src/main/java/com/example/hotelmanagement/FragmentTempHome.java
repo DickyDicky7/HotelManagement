443e8d19204a1fd49fcfa2c625196bd004837e7b
21520147-PhamTuanAnh <21520147@gm.uit.edu.vn>
2023-05-25 12:27:18 +0700

    :recycle: clean
    Signed-off-by: 21520147-PhamTuanAnh <21520147@gm.uit.edu.vn>

--- app/src/main/java/com/example/hotelmanagement/FragmentTempHome.java
+++ app/src/main/java/com/example/hotelmanagement/FragmentTempHome.java
@@ -10,6 +10,11 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import com.auth0.android.Auth0;
+import com.auth0.android.authentication.AuthenticationException;
+import com.auth0.android.callback.Callback;
+import com.auth0.android.provider.WebAuthProvider;
+import com.auth0.android.result.Credentials;
 import com.example.hotelmanagement.databinding.FragmentTempHomeBinding;
 
 public class FragmentTempHome extends Fragment {
@@ -26,6 +31,21 @@ public class FragmentTempHome extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        binding.button6.setOnClickListener(_view_ -> {
+            Auth0 account = new Auth0(getString(R.string.com_auth0_client_id), getString(R.string.com_auth0_domain));
+            WebAuthProvider.login(account).start(requireContext(), new Callback<Credentials, AuthenticationException>() {
+                @Override
+                public void onSuccess(Credentials credentials) {
+                    System.out.println(credentials.getUser().getEmail());
+                }
+
+                @Override
+                public void onFailure(@NonNull AuthenticationException e) {
+                    System.out.println(e);
+                }
+            });
+        });
 
         binding.button.setOnClickListener(_view -> {
             NavHostFragment.findNavController(this).navigate(R.id.action_fragmentTempHome_to_fragmentAddRoom);
