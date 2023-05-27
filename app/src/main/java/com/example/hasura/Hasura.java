7b0236f06077ccee270a14ee92e816b6196f10a3
21520147-PhamTuanAnh <21520147@gm.uit.edu.vn>
2023-05-27 22:52:25 +0700

    :recycle: clean
    Signed-off-by: 21520147-PhamTuanAnh <21520147@gm.uit.edu.vn>

--- app/src/main/java/com/example/hasura/Hasura.java
+++ app/src/main/java/com/example/hasura/Hasura.java
@@ -99,7 +99,7 @@ public class Hasura {
                     .addHeader
                             (
                                     "Authorization",
-                                    authorizationToken
+                                    "Bearer" + " " + authorizationToken
                             )
                     .build();
 
