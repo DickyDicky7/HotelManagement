d09f7068231edac1372c6c27be33b6e07a40861a
21520147-PhamTuanAnh <21520147@gm.uit.edu.vn>
2023-06-07 01:58:51 +0700

    :recycle: clean
    Signed-off-by: 21520147-PhamTuanAnh <21520147@gm.uit.edu.vn>

--- app/src/main/java/com/example/hotelmanagement/ActivityMain.java
+++ app/src/main/java/com/example/hotelmanagement/ActivityMain.java
@@ -3,15 +3,8 @@ package com.example.hotelmanagement;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.example.hotelmanagement.databinding.ActivityMainBinding;
-import com.example.hotelmanagement.viewmodels.BillViewModel;
-import com.example.hotelmanagement.viewmodels.GuestKindViewModel;
-import com.example.hotelmanagement.viewmodels.GuestViewModel;
-import com.example.hotelmanagement.viewmodels.RentalFormViewModel;
-import com.example.hotelmanagement.viewmodels.RoomKindViewModel;
-import com.example.hotelmanagement.viewmodels.RoomViewModel;
 
 public class ActivityMain extends AppCompatActivity {
 
@@ -22,19 +15,6 @@ public class ActivityMain extends AppCompatActivity {
         ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        BillViewModel billViewModel = new ViewModelProvider(this).get(BillViewModel.class);
-        RoomViewModel roomViewModel = new ViewModelProvider(this).get(RoomViewModel.class);
-        GuestViewModel guestViewModel = new ViewModelProvider(this).get(GuestViewModel.class);
-        RoomKindViewModel roomKindViewModel = new ViewModelProvider(this).get(RoomKindViewModel.class);
-        GuestKindViewModel guestKindViewModel = new ViewModelProvider(this).get(GuestKindViewModel.class);
-        RentalFormViewModel rentalFormViewModel = new ViewModelProvider(this).get(RentalFormViewModel.class);
-
-        billViewModel.loadData();
-        roomViewModel.loadData();
-        guestViewModel.loadData();
-        roomKindViewModel.loadData();
-        guestKindViewModel.loadData();
-        rentalFormViewModel.loadData();
     }
 
 }
--- app/src/main/java/com/example/hotelmanagement/fragments/commons/FragmentLogin.java
+++ app/src/main/java/com/example/hotelmanagement/fragments/commons/FragmentLogin.java
@@ -8,11 +8,18 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.hasura.Hasura;
 import com.example.hotelmanagement.R;
 import com.example.hotelmanagement.databinding.FragmentLoginBinding;
+import com.example.hotelmanagement.viewmodels.BillViewModel;
+import com.example.hotelmanagement.viewmodels.GuestKindViewModel;
+import com.example.hotelmanagement.viewmodels.GuestViewModel;
+import com.example.hotelmanagement.viewmodels.RentalFormViewModel;
+import com.example.hotelmanagement.viewmodels.RoomKindViewModel;
+import com.example.hotelmanagement.viewmodels.RoomViewModel;
 
 import java.util.function.Consumer;
 
@@ -30,8 +37,24 @@ public class FragmentLogin extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
         binding.btnSignIn.setOnClickListener(_view_ -> {
-            Consumer<Void> onSuccessCallback = unused -> NavHostFragment.findNavController(this).navigate(R.id.action_fragmentLogin_to_fragmentHome);
+
+            Consumer<Void> onSuccessCallback = unused -> {
+                BillViewModel billViewModel = new ViewModelProvider(requireActivity()).get(BillViewModel.class);
+                RoomViewModel roomViewModel = new ViewModelProvider(requireActivity()).get(RoomViewModel.class);
+                GuestViewModel guestViewModel = new ViewModelProvider(requireActivity()).get(GuestViewModel.class);
+                RoomKindViewModel roomKindViewModel = new ViewModelProvider(requireActivity()).get(RoomKindViewModel.class);
+                GuestKindViewModel guestKindViewModel = new ViewModelProvider(requireActivity()).get(GuestKindViewModel.class);
+                RentalFormViewModel rentalFormViewModel = new ViewModelProvider(requireActivity()).get(RentalFormViewModel.class);
+                billViewModel.loadData();
+                roomViewModel.loadData();
+                guestViewModel.loadData();
+                roomKindViewModel.loadData();
+                guestKindViewModel.loadData();
+                rentalFormViewModel.loadData();
+                NavHostFragment.findNavController(this).navigate(R.id.action_fragmentLogin_to_fragmentHome);
+            };
             Consumer<Void> onFailureCallback = null;
             Hasura.configAuth0
                     (
@@ -45,7 +68,9 @@ public class FragmentLogin extends Fragment {
                             onSuccessCallback,
                             onFailureCallback
                     );
+
         });
+
     }
 
     @Override
