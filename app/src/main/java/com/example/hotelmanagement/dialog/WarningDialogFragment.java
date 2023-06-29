fad1b76c4e4d8697ebbb85e3c83828b24bc03334
21520147-PhamTuanAnh <21520147@gm.uit.edu.vn>
2023-06-29 16:59:33 +0700

    :art:
    Signed-off-by: 21520147-PhamTuanAnh <21520147@gm.uit.edu.vn>

---
+++ app/src/main/java/com/example/hotelmanagement/dialog/WarningDialogFragment.java
@@ -0,0 +1,79 @@
+package com.example.hotelmanagement.dialog;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.FragmentManager;
+
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
+import com.example.hotelmanagement.databinding.WarningDialogBinding;
+
+import java.util.function.Consumer;
+
+public class WarningDialogFragment extends DialogFragment {
+
+    @NonNull
+    protected String message;
+    @NonNull
+    protected Answer answers;
+    @NonNull
+    protected Consumer<Answer> onCancelHandler;
+
+    public WarningDialogFragment(@NonNull String message, @NonNull Consumer<Answer> onCancelHandler) {
+        this.message = message;
+        this.answers = Answer.HUH;
+        this.onCancelHandler = onCancelHandler;
+    }
+
+    public static void newOne(@NonNull FragmentManager fragmentManager, @NonNull String tag, @NonNull String message, @NonNull Consumer<Answer> onCancelHandler) {
+        WarningDialogFragment warningDialogFragment = new WarningDialogFragment(message, onCancelHandler);
+        warningDialogFragment.showNow(fragmentManager, tag);
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        WarningDialogBinding binding = WarningDialogBinding.inflate(getLayoutInflater());
+        AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
+        builder.setView(binding.getRoot());
+        AlertDialog alertDialog = builder.create();
+
+        binding.notiWarningMsg.setText(message);
+
+        binding.notiWarningOK.setOnClickListener(view -> {
+            answers = Answer.YES;
+            alertDialog.cancel();
+        });
+
+        binding.notiWarningBtnClose.setOnClickListener(view -> {
+            answers = Answer.NAH;
+            alertDialog.cancel();
+        });
+
+        alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        YoYo.with(Techniques.Bounce).duration(1000).playOn(alertDialog.getWindow().getDecorView());
+
+        return alertDialog;
+    }
+
+    @Override
+    public void onCancel(@NonNull DialogInterface dialog) {
+        super.onCancel(dialog);
+        onCancelHandler.accept(answers);
+    }
+
+    public enum Answer {
+        YES,
+        NAH,
+        HUH,
+    }
+
+}
